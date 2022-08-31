7439e448c85b01d36cb70ea28e3c5017a28f712f
menoliu <nemoliu123@gmail.com>
2022-08-31 13:55:30 -0400

    import required components

---
+++ src/xeisd/components/parser.py
@@ -0,0 +1,282 @@
+"""
+Main logic for parsing experimental and back-calculated raw outputs.
+Accepts primarily NMR-STAR formatted files for solution data.
+
+USAGE EXAMPLE:
+    For NMR-STAR files, it must be the data between the `loop_` and 
+    `stop_` codes without new lines. The easiest way is to just
+    copy the block of data from NMR-STAR file from the BMRB
+    to a new text file.
+    
+    For example an exerpt from entry 4155:
+    _Atom_chem_shift.ID
+    _Atom_chem_shift.Assembly_atom_ID
+    _Atom_chem_shift.Entity_assembly_ID
+    _Atom_chem_shift.Entity_assembly_asym_ID
+    _Atom_chem_shift.Entity_ID
+    _Atom_chem_shift.Comp_index_ID
+    _Atom_chem_shift.Seq_ID
+    _Atom_chem_shift.Comp_ID
+    _Atom_chem_shift.Atom_ID
+    _Atom_chem_shift.Atom_type
+    _Atom_chem_shift.Atom_isotope_number
+    _Atom_chem_shift.Val
+    _Atom_chem_shift.Val_err
+    1     .   2   .   1   2    2    MET   C    C   13   176.341   .
+    2     .   2   .   1   2    2    MET   CA   C   13   55.634    .
+    3     .   2   .   1   2    2    MET   CB   C   13   32.613    .
+    4     .   2   .   1   3    3    GLU   H    H   1    8.781     .
+    5     .   2   .   1   3    3    GLU   C    C   13   176.158   .
+    6     .   2   .   1   3    3    GLU   CA   C   13   56.8      .
+    7     .   2   .   1   3    3    GLU   CB   C   13   29.905    .
+    8     .   2   .   1   3    3    GLU   N    N   15   122.712   .
+    9     .   2   .   1   4    4    ALA   H    H   1    8.419     .
+    10    .   2   .   1   4    4    ALA   C    C   13   177.602   .
+    11    .   2   .   1   4    4    ALA   CA   C   13   52.395    .
+
+Currently accepting the following formats for experimental data:
+    SAXS: SASBDB (curve .DAT), CUSTOM
+    Chemical shift: NMR-STAR
+    FRET: (ask greg/claudiu for the "standard")
+    J-Couplings: NMR-STAR (coupling constants), CUSTOM
+    NOE: NMR-STAR (homonuclear/distances), CUSTOM
+    PRE: NMR STAR (changes in R2 or as distance (use NOE)), CUSTOM
+    RDC: NMR-STAR, CUSTOM
+    Rh: CUSTOM (single values)
+
+CUSTOM format:
+    For cases where experimental data is not found on BMRB/SASDB, the
+    formatting is delineated by comma (,) and the first line dictates
+    what each column represents (i.e. pandas DataFrame format).
+    
+    Examples for each data-type has been provided in the ``/exp_examples`` folder.
+    
+    SAXS: index,value,error
+    CS: index,resnum,atomname,value,error
+    FRET: TBD
+    JC: index,resnum,value,error
+    NOE: index,value,lower,upper,error*
+    PRE: index,value,lower,upper,error*
+    RDC: index,resnum,value,error
+    Rh: index,resnum,value,error
+    
+    For NOE and PRE, error* can be provided instead of lower+upper.
+    
+TODO: FRET and Rh have custom "sigma" values in the Stack, be advised.
+NOTE: misc. information such as res1, atom1, etc. are for back-calculations.
+"""
+import numpy as np
+import pandas as pd
+
+from xeisd.components import (
+    exp_idx,
+    exp_val,
+    exp_err,
+    exp_atmID,
+    exp_max,
+    exp_min,
+    parse_mode_exp,
+    saxs_name,
+    cs_name,
+    fret_name,
+    jc_name,
+    noe_name,
+    pre_name,
+    rdc_name,
+    rh_name,
+    )
+
+
+class Stack():
+    def __init__(self, name, data, sigma=None, mu=None):
+        """
+        Initialize stack of experimental or back-calculated
+        data.
+
+        Parameters
+        ----------
+        name : string
+            Name of experimental datatype.
+        
+        pd.DataFrame
+            Containing atoms, values, and errors for the 3 columns
+        """
+        self.name = name
+        self.data = data
+        self.sigma = sigma
+        self.mu = mu
+
+
+def parse_saxs_data(fpath):
+    # TODO: might have to change what index is
+    index = []
+    value = []
+    error = []
+    
+    with open(fpath) as f:
+        lines = f.readlines()
+        first = lines[0].split(',')
+        if first[0] == exp_idx:
+            raise TypeError
+        
+        for line in lines:
+            splitted = line.split()
+            if type(splitted[0]) is float:
+                index.append(splitted[0])
+                value.append(splitted[1])
+                error.append(splitted[2])
+    
+    return pd.DataFrame({exp_idx: index, exp_val: value, exp_err: error})
+
+
+def parse_cs_data(fpath):
+    index = []
+    atoms = []
+    values = []
+    errors = []
+    
+    with open(fpath) as f:
+        lines = f.readlines()
+        first = lines[0].split(',')
+        if first[0] == exp_idx:
+            raise TypeError
+        
+        for i, line in enumerate(lines):
+            if "_" in line:
+                dtype = line.split(".")[1].strip()
+                if dtype == "Val": data_idx = i
+                elif dtype == "Val_err": error_idx = i
+                elif dtype == "Atom_ID": atom_idx = i
+            else:
+                start_idx = i
+                break
+        
+        idx = 1
+        for idx in range(start_idx, len(lines)):
+            splitted = lines[idx].split()
+            
+            index.append(idx)
+            atoms.append(splitted[atom_idx])
+            values.append(float(splitted[data_idx]))
+            errors.append(float(splitted[error_idx]))
+            idx += 1
+            
+    return pd.DataFrame({exp_idx: index, exp_atmID: atoms, exp_val: values, exp_err: errors})
+
+
+# Good for NMR-STAR formatted JC, RDC, NOE, PRE
+def parse_nmrstar_data(fpath, type=None):
+    index = []
+    values = []
+    upper = []
+    lower = []
+    errors = []
+    
+    with open(fpath) as f:
+        lines = f.readlines()
+        first = lines[0].split(',')
+        if first[0] == exp_idx:
+            raise TypeError
+        
+        for i, line in enumerate(lines):
+            if "_" in line:
+                dtype = line.split(".")[1].strip()
+                if dtype == "Val": data_idx = i
+                elif dtype == "Val_err": error_idx = i
+                if type == noe_name or type == pre_name:
+                    if dtype == "Val_max": max_idx = i
+                    elif dtype == "Val_min": min_idx = i
+            else:
+                start_idx = i
+                break
+        
+        idx=1
+        for idx in range(start_idx, len(lines)):
+            splitted = lines[idx].split()
+            
+            index.append(idx)
+            values.append(float(splitted[data_idx]))
+            errors.append(float(splitted[error_idx]))
+            idx += 1
+            
+            if type == noe_name or type == pre_name:
+                max = splitted[max_idx]
+                min = splitted[min_idx]
+                if max == ".": max = 0.0
+                if min == ".": min = 0.0
+                upper.append(float(max))
+                lower.append(float(min))
+    
+    if type == noe_name or type == pre_name:
+        return pd.DataFrame({exp_idx: index, exp_val: values, exp_max: upper, exp_min: lower, exp_err: errors})
+    
+    return pd.DataFrame({exp_idx: index, exp_val: values, exp_err: errors})
+
+
+def parse_data(filenames, mode, bc_errors={}):
+    """
+    Main function to read/parse all experimental and back-calculated files.
+
+    Parameters
+    ----------
+    filenames : dict
+        Dictionary of modules with their relative path
+        to the data file. First key-layer has all of the
+        different experimental modules eisd can handel.
+    
+    mode : str
+        Parameter must be one of the following:
+        - 'exp' = experimental data
+        - 'bc' = back-calculated data
+        
+    Returns
+    -------
+    parsed : dict
+        Dictionary of properties with their pandas dataframe.
+    
+    errlogs : list
+        List of possible messages to display for the user.    
+    """
+    parsed = {}
+    errlogs = []
+    data = pd.DataFrame(dtype=np.float)
+    
+    # Parse experimental data
+    if mode == parse_mode_exp:
+        for module in filenames:
+            try:
+                if module == saxs_name:
+                    data = parse_saxs_data(filenames[module])
+                elif module == cs_name:
+                    data = parse_cs_data(filenames[module])
+                elif module == fret_name:
+                    # do fret parsing
+                    None
+                elif module == jc_name:
+                    data = parse_nmrstar_data(filenames[module])
+                elif module == noe_name:
+                    data = parse_nmrstar_data(filenames[module], noe_name)
+                elif module == pre_name:
+                    data = parse_nmrstar_data(filenames[module], pre_name)
+                elif module == rdc_name:
+                    data = parse_nmrstar_data(filenames[module])
+                elif module == rh_name:
+                    data = pd.read_csv(filenames[module], delimiter=',')
+            except TypeError:
+                data = pd.read_csv(filenames[module], delimiter=',')
+                     
+            parsed[module] = Stack(module, data, None, None)
+    # Parse back calculated data
+    # NOTE: for these examples, the back-calculator interface saves
+    # data in .txt but CSV formatted
+    # TODO: in the future it may be worthwhile to consider one
+    # .JSON file to house all of the back-calculated data...
+    else:
+        for module in filenames:
+            try:
+                data = pd.read_csv(filenames[module], delimiter=',')
+                parsed[module] = Stack(module, data, None, None)
+            except Exception as e:
+                errlogs.append(e)
+            
+    return parsed, errlogs
---
+++ src/xeisd/components/scorers.py
@@ -0,0 +1,496 @@
+"""
+Module contains all functions required to perform a single property
+maximum loglike optimization.
+
+Inspired/imported from:
+* https://github.com/THGLab/X-EISD/blob/master/eisd/scorers.py
+* https://github.com/Oufan75/X-EISD/blob/master/eisd/scorers.py
+"""
+import numpy as np
+
+from xeisd.components import(
+    exp_idx,
+    exp_val,
+    exp_err,
+    exp_atmID,
+    exp_max,
+    exp_min,
+    saxs_name,
+    cs_name,
+    fret_name,
+    jc_name,
+    noe_name,
+    pre_name,
+    rdc_name,
+    rh_name
+    )
+
+
+def calc_opt_params(beta, exp, exp_sig, sig):
+    opt_params = np.zeros(beta.shape)
+    if not np.any(exp_sig==0):
+        ratio = (sig ** 2.0) / (exp_sig ** 2.0)
+        opt_params = (ratio * (exp - beta)) / (1.0 + ratio)
+    return opt_params
+
+
+def normal_loglike(x, mu, sig, gamma=1.0):
+    # Allow for cases where one/more sig are zero
+    # all assigned to zero for now
+    # modified by Oufan Zhang @Oufan75
+    logp = np.zeros(x.shape)
+    if not np.any(sig==0):
+        exp_val = -gamma * ((x - mu) ** 2.0) / (2.0 * (sig ** 2.0))
+        pre_exp = 1.0 / (np.sqrt(2.0 * np.pi * (sig ** 2.0)))
+        logp = np.log(pre_exp * np.exp(exp_val))
+    return logp
+
+
+def calc_score(beta, exp, exp_sig, sig, opt_params, gamma=1.0):
+    f_q = normal_loglike(opt_params, 0, sig, gamma)
+    err = exp - opt_params - beta
+    f_err = normal_loglike(err, 0, exp_sig, gamma)
+    f = f_q + f_err
+    f_comps = [f_q, f_err]
+    return f, f_comps
+
+
+def calc_gamma(len_saxs, num_res, qmax, qmin):
+    # Generalizable way to calculate gamma parameter
+    # accournding to shannon theory
+    N_q = len_saxs
+    N_s = num_res * (qmax - qmin) / np.pi
+    
+    return N_s / N_q
+
+
+def vect_calc_opt_params_jc(
+    alpha1,
+    alpha2,
+    exp_j,
+    exp_sig,
+    mus,
+    sigs,
+    ):
+    """
+    Takes an average phi value and experimental data point
+    calculates optimal A, B, C parameters
+    """
+    a = np.zeros((alpha1.shape[0], 3, 3))
+    b = np.zeros((alpha1.shape[0], 3))
+
+    a[:, 0, 0] = 1.0 / (sigs[0] ** 2.0) + ((alpha2 / exp_sig) ** 2.0)
+    a[:, 1, 1] = 1.0 / (sigs[1] ** 2.0) + ((alpha1 / exp_sig) ** 2.0)
+    a[:, 2, 2] = 1.0 / (sigs[2] ** 2.0) + 1.0 / (exp_sig ** 2.0)
+
+    a[:, 0, 1] = alpha1 * alpha2 / (exp_sig ** 2.0)
+    a[:, 1, 0] = alpha1 * alpha2 / (exp_sig ** 2.0)
+    a[:, 0, 2] = alpha2 / (exp_sig ** 2.0)
+    a[:, 2, 0] = alpha2 / (exp_sig ** 2.0)
+    a[:, 1, 2] = alpha1 / (exp_sig ** 2.0)
+    a[:, 2, 1] = alpha1 / (exp_sig ** 2.0)
+
+    b[:, 0] = mus[0] / (sigs[0] ** 2.0) + exp_j * alpha2 / (exp_sig ** 2)
+    b[:, 1] = mus[1] / (sigs[1] ** 2.0) + exp_j * alpha1 / (exp_sig ** 2)
+    b[:, 2] = mus[2] / (sigs[2] ** 2.0) + exp_j / (exp_sig ** 2)
+
+    opt_params = np.array([np.linalg.solve(a[i], b[i]) for i in range(a.shape[0])])  # shape: (47,3)
+
+    return opt_params
+
+
+def vect_calc_score_jc(
+    alpha1,
+    alpha2,
+    exp_j,
+    exp_sig,
+    opt_params,
+    mus,
+    sigs
+    ):
+    """
+    Calculates score for a single phi angle/residue, 
+    can be used on a single protein or ensemble.
+    
+    Returns
+    -------
+    f : float
+        Total score calculated
+    
+    f_comps : array
+        [f_a, f_b, f_c, f_err]
+    """
+    f_a = normal_loglike(opt_params[:,0], mus[0], sigs[0])
+    f_b = normal_loglike(opt_params[:,1], mus[1], sigs[1])
+    f_c = normal_loglike(opt_params[:,2], mus[2], sigs[2])
+    err = exp_j - opt_params[:,0] * alpha2 - opt_params[:,1] * alpha1 - opt_params[:,2]
+    f_err = normal_loglike(err, 0 , exp_sig)
+    f = f_a + f_b + f_c + f_err
+    f_comps = [f_a, f_b, f_c, f_err]
+    
+    return f, f_comps
+
+
+# ------------------------------------------------------------
+# Below we have the functions to score each ensemble
+# by a specific experimental modules
+# ------------------------------------------------------------
+
+
+def saxs_optimization_ensemble(
+    exp_data,
+    bc_data,
+    indices,
+    ens_size,
+    nres,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    # prepare data
+    exp_saxs = exp_data[saxs_name].data[exp_val].values
+    exp_sigma = exp_data[saxs_name].data[exp_err].values
+    
+    if indices is None:
+        bc_saxs = old_vals - \
+            (bc_data[saxs_name].data.values[popped_structure, :] - \
+            bc_data[saxs_name].data.values[new_index, :] ) / ens_size
+        
+    else:
+        bc_ensemble = bc_data[saxs_name].data.values[indices, :]
+        bc_saxs = np.mean(bc_ensemble, axis=0)
+        
+    # optimization
+    opt_params = calc_opt_params(bc_saxs, exp_saxs, exp_sigma, bc_data[saxs_name].sigma)
+    
+    g = calc_gamma(
+        exp_data[saxs_name].data.shape[0],
+        nres,
+        np.max(exp_data[saxs_name].data[exp_idx]),
+        np.min(exp_data[saxs_name].data[exp_idx])
+        )
+    
+    f, f_comps = calc_score(bc_saxs, exp_saxs, exp_sigma, bc_data[saxs_name].sigma, opt_params, gamma=g)
+
+    error = (exp_saxs - bc_saxs) ** 2 
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+    
+    return rmse, total_score, bc_saxs, error
+
+
+def cs_optimization_ensemble(
+    exp_data,
+    bc_data,
+    ens_size,
+    indices,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    """
+    Main logic for chemical shift scoring.
+
+    Parameters
+    ----------
+    exp_data : dict
+        Dictionary of experimental values of chemical shifts.
+        First key-layer indicates type of CS (C, CA, CB, N, H, HA).
+        For each key, there exists 2 values, the CS assignment [0]
+        and the experimental error associated [1].
+    
+    bc_data : dict
+        Dictionary of back-calculated values of chemical shifts.
+        Format follows `exp_data`.
+    
+    indices : list
+        List of indices of experimental and back-calculated data
+        to include. Defaults to None.
+        # !!! note here, may not be required based on new logic
+    
+    """
+    # TODO: if incorrect shape, shave off values from bc_data
+    # to match exp_data
+    exp_cs = exp_data[cs_name].data[exp_val].values
+    exp_sigma = exp_data[cs_name].data[exp_err].values
+    atom_types = exp_data[cs_name].data[exp_atmID].values
+    
+    if indices is None:
+        bc_cs = old_vals - \
+            (bc_data[cs_name].data.values[popped_structure, :] - \
+            bc_data[cs_name].data.values[new_index, :]) / ens_size
+    else:
+        bc_ensemble = bc_data[cs_name].data.values[indices, :]
+        bc_cs = np.mean(bc_ensemble, axis=0)
+        
+    bc_sigma = np.array([bc_data[cs_name].sigma[atom_type] for atom_type in atom_types])
+    
+    opt_params = calc_opt_params(bc_cs, exp_cs, exp_sigma, bc_sigma)
+    f, f_comps = calc_score(bc_cs, exp_cs, exp_sigma, bc_sigma, opt_params)
+    
+    error = (exp_cs - bc_cs) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+    
+    return rmse, total_score, bc_cs, error
+
+
+# NOTE: scoring may change depends on what we hear from Gradinaru group
+def fret_optimization_ensemble(
+    exp_data,
+    bc_data,
+    ens_size,
+    indices,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    """
+    Main logic for fret scoring module
+    """
+    # prepare data
+    exp = exp_data[fret_name].data
+    exp_sigma = exp_data[fret_name].sigma
+
+    if indices is None:
+        bc = old_vals - \
+            (bc_data[fret_name].data.values[popped_structure, :] - \
+            bc_data[fret_name].data.values[new_index, :]) / ens_size
+    else:
+        bc_ensemble = bc_data[fret_name].data.values[indices, :]
+        bc = np.mean(bc_ensemble, axis=0)
+
+    bc_sigma = bc_data[fret_name].sigma
+
+    # optimization
+    opt_params = calc_opt_params(bc, exp, exp_sigma, bc_sigma)
+    f, f_comps = calc_score(bc, exp, exp_sigma, bc_sigma, opt_params)
+
+    error = (exp - bc) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+
+    return rmse, total_score, bc, error
+
+    
+def jc_optimization_ensemble(
+    exp_data,
+    bc_data,
+    ens_size,
+    indices,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    """
+    Main logic for J-coupling scoring.
+    """
+    exp = exp_data[jc_name].data[exp_val].values
+    exp_sigma = exp_data[jc_name].data[exp_err].values
+
+    if indices is None:
+        pop_alpha = bc_data[jc_name].data.values[popped_structure, :]
+        add_alpha = bc_data[jc_name].data.values[new_index, :]
+
+        bc_alpha1 = old_vals[0] - (pop_alpha - add_alpha) / ens_size
+        bc_alpha2 = old_vals[1] - (np.square(pop_alpha) - np.square(add_alpha)) / ens_size
+        
+        assert bc_alpha1.shape == bc_alpha2.shape
+    else:
+        bc_ensemble_alpha1 = bc_data[jc_name].data.values[indices, :]
+        bc_alpha1 = np.mean(bc_ensemble_alpha1, axis=0)
+
+        bc_ensemble_alpha2 = np.square(bc_data[jc_name].data.values[indices, :])
+        bc_alpha2 = np.mean(bc_ensemble_alpha2, axis=0)
+
+    bc_sigma = [bc_data[jc_name].sigma[i] for i in ["A", "B", "C"]]
+    bc_mu = [bc_data[jc_name].mu[i] for i in ["A", "B", "C"]]
+
+    opt_params = vect_calc_opt_params_jc(
+        bc_alpha1,
+        bc_alpha2, 
+        exp,
+        exp_sigma,
+        bc_mu,
+        bc_sigma
+        )
+    
+    f, f_comps = vect_calc_score_jc(
+        bc_alpha1,
+        bc_alpha2,
+        exp,
+        exp_sigma,
+        opt_params,
+        bc_mu,
+        bc_sigma
+        )
+    
+    error = (opt_params[:,0] * bc_alpha2 + opt_params[:,1] * bc_alpha1 + opt_params[:,2] - exp) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    scores = np.sum(f)
+    jcoup_vals = list(opt_params[:,0] * bc_alpha2 + opt_params[:,1] * bc_alpha1 + opt_params[:,2])
+
+    return rmse, scores, jcoup_vals, [bc_alpha1, bc_alpha2]
+
+
+def noe_optimization_ensemble(
+    exp_data,
+    bc_data,
+    indices,
+    ens_size,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    """
+    Main logic for NOE scoring.
+    """
+    exp_distance = exp_data[noe_name].data[exp_val].values
+    
+    # Either `upper,lower` or `error` is required for exp_sigma
+    try:
+        upper_bound_value = exp_data[noe_name].data[exp_max].values 
+        lower_bound_value = exp_data[noe_name].data[exp_min].values
+        
+        assert exp_distance.shape == upper_bound_value.shape == lower_bound_value.shape
+        
+        range_val = upper_bound_value + lower_bound_value
+        exp_sigma = range_val / 2.0
+    except ValueError:
+        exp_sigma = exp_data[noe_name].data[exp_sigma].values
+    
+    # load long range noe bc index
+    # calculating inverse 6 average
+    if indices is None:
+        popped = np.power(bc_data[noe_name].data.values[popped_structure], -6.0)
+        added = np.power(bc_data[noe_name].data.values[new_index], -6.0)
+        # check that "ens_size" could be 100. ?
+        avg_distance = (np.power(old_vals, -6.0) * ens_size - (popped - added) ) / ens_size
+        avg_distance = np.power(avg_distance, (-1. / 6.))
+    else:
+        bc_ensemble = np.power(bc_data[noe_name].data.values[indices], -6.0)
+        avg_distance = np.power(np.mean(bc_ensemble, axis=0), (-1. / 6.))
+    
+    # optimization
+    opt_params = calc_opt_params(avg_distance, exp_distance, exp_sigma, bc_data[noe_name].sigma)
+    f, f_comps = calc_score(avg_distance, exp_distance, exp_sigma, bc_data[noe_name].sigma, opt_params)
+    
+    error = (exp_distance - avg_distance) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+    
+    return rmse, total_score, avg_distance, error
+
+
+def pre_optimization_ensemble(
+    exp_data,
+    bc_data,
+    indices,
+    ens_size,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    """
+    Main logic for PRE scoring function.
+    """
+    # prepare data
+    exp_distance = exp_data[pre_name].data[exp_val].values
+    # Either `upper,lower` or `error` is required for exp_sigma
+    try:
+        upper_bound_value = exp_data[pre_name].data[exp_max].values 
+        lower_bound_value = exp_data[pre_name].data[exp_min].values
+        
+        assert exp_distance.shape == upper_bound_value.shape == lower_bound_value.shape
+        
+        range_val = upper_bound_value + lower_bound_value
+        exp_sigma = range_val / 2.0
+    except ValueError:
+        exp_sigma = exp_data[pre_name].data[exp_sigma].values
+    
+    if indices is None:
+        popped = np.power(bc_data[pre_name].data.values[popped_structure, :], -6.0)
+        added = np.power(bc_data[pre_name].data.values[new_index, :], -6.0)
+        avg_distance = (np.power(old_vals, -6.0) * ens_size - (popped - added)) / ens_size
+        avg_distance = np.power(avg_distance, (-1. / 6.))
+    else:
+        bc_ensemble = np.power(bc_data[pre_name].data.values[indices, :], -6.0)
+        avg_distance = np.power(np.mean(bc_ensemble, axis=0), (-1./6.))
+    
+        # optimization
+    opt_params = calc_opt_params(avg_distance, exp_distance, exp_sigma, bc_data[pre_name].sigma)
+    f, f_comps = calc_score(avg_distance, exp_distance, exp_sigma, bc_data[pre_name].sigma, opt_params)
+    
+    error = (exp_distance - avg_distance) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+    
+    return rmse, total_score, avg_distance, error 
+
+
+def rdc_optimization_ensemble(
+    exp_data,
+    bc_data,
+    indices,
+    ens_size,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    # prepare data
+    exp = exp_data[rdc_name].data[exp_val].values
+    exp_sigma = exp_data[rdc_name].data[exp_err].values
+    
+    assert exp.shape == exp_sigma.shape
+
+    if indices is None:
+        bc = old_vals - \
+            (bc_data[rdc_name].data.values[popped_structure, :] - \
+            bc_data[rdc_name].data.values[new_index, :]) / ens_size
+    else:
+        bc_ensemble = bc_data[rdc_name].data.values[indices, :]
+        bc = np.mean(bc_ensemble, axis=0)
+
+    # optimization
+    opt_params = calc_opt_params(bc, exp, exp_sigma, bc_data[rdc_name].sigma)
+    f, f_comps = calc_score(bc, exp, exp_sigma, bc_data[rdc_name].sigma, opt_params)
+    
+    error = (exp - bc) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+
+    return rmse, total_score, bc, error
+
+
+def rh_optimization_ensemble(
+    exp_data,
+    bc_data,
+    indices,
+    ens_size,
+    old_vals=None,
+    popped_structure=None,
+    new_index=None,
+    ):
+    # prepare data
+    exp = exp_data[rh_name].data
+    exp_sigma = exp_data[rh_name].sigma
+
+    if indices is None:
+        bc = old_vals - \
+            (bc_data[rh_name].data.values[popped_structure, :] - \
+            bc_data[rh_name].data.values[new_index, :]) / ens_size
+    else:
+        bc_ensemble = bc_data[rh_name].data.values[indices, :]
+        bc = np.mean(bc_ensemble, axis=0)
+
+    bc_sigma = bc_data[rh_name].sigma
+
+    # optimization
+    opt_params = calc_opt_params(bc, exp, exp_sigma, bc_sigma)
+    f, f_comps = calc_score(bc, exp, exp_sigma, bc_sigma, opt_params)
+    
+    error = (exp - bc) ** 2.0
+    rmse = np.mean(error) ** 0.5
+    total_score = np.sum(f)
+
+    return rmse, total_score, bc[0], error
